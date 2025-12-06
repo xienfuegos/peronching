81fbca275b93f331febf637c7ecfe177c98b2597
Xienfuegos <consorcio.tv@gmail.com>
2025-12-05 22:31:55 -0300

    feat: add Vercel Analytics

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from "@vercel/analytics/react"
 import App from './App';
 import './index.css';
 
@@ -12,5 +13,6 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
